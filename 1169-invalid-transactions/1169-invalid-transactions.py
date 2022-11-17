e2431e722aedb15ead9eaad72de2046b89514e87
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2022-11-17 03:42:39 +0000

    Time: 126 ms (78.28%), Space: 14.7 MB (35.80%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -1,20 +1,23 @@
 class Solution:               
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        res = []
-        trans = [x.split(',') for x in transactions]
-        for i in range(len(trans)):
-            name, time, money, location = trans[i]
-            time = int(time)
-            money = int(money)
-            if money > 1000:
-                res.append(transactions[i])
-                continue
-            for j in range(len(trans)):
-                if i != j:
-                    name1, time1, money1, location1 = trans[j]
-                    time1=int(time1)
-                    if name1 == name and abs(time1-time) <= 60 and location1 != location:
-                        res.append(transactions[i])
-                        break
-        return res
+        ans = []
+        nameToTranses = defaultdict(list)
+
+        for t in transactions:
+          name, time, amount, city = t.split(',')
+          time, amount = int(time), int(amount)
+          nameToTranses[name].append({'time': time, 'city': city})
+
+        for t in transactions:
+          name, time, amount, city = t.split(',')
+          time, amount = int(time), int(amount)
+          if amount > 1000:
+            ans.append(t)
+          elif name in nameToTranses:
+            for sameName in nameToTranses[name]:
+              if abs(sameName['time'] - time) <= 60 and sameName['city'] != city:
+                ans.append(t)
+                break
+
+        return ans
         
