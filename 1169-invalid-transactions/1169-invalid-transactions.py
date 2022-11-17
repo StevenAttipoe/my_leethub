077bbce33569314db6a38c5a4e03781d8af4f91f
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2022-11-17 03:52:59 +0000

    Time: 143 ms (66.73%), Space: 14.8 MB (17.51%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -1,23 +1,35 @@
-class Solution:               
+class Solution:   
+    class Transaction:
+        def __init__(self, data):
+            self.name = data[0]
+            self.time = int(data[1])
+            self.amount = int(data[2])
+            self.city = data[3]
+            
+        def toString(self):
+            return self.name + "," + str(self.time) + "," + str(self.amount) + "," +self.city
+            
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        ans = []
-        nameToTranses = defaultdict(list)
+        transMap = {}
+        res = []
+        for transaction in transactions:
+            currTrans = Solution.Transaction(transaction.split(","))
+            if currTrans.name in transMap:
+                transMap[currTrans.name].append(currTrans)
+            else:
+                transMap[currTrans.name] = [currTrans]
 
-        for t in transactions:
-            name, time, amount, city = t.split(',')
-            time, amount = int(time), int(amount)
-            nameToTranses[name].append({'time': time, 'city': city})
-
-        for t in transactions:
-            name, time, amount, city = t.split(',')
-            time, amount = int(time), int(amount)
-            if amount > 1000:
-                ans.append(t)
-            elif name in nameToTranses:
-                for sameName in nameToTranses[name]:
-                    if abs(sameName['time'] - time) <= 60 and sameName['city'] != city:
-                        ans.append(t)
+        for transaction in transactions:
+            currTrans = Solution.Transaction(transaction.split(","))
+            if currTrans.amount > 1000:
+                res.append(currTrans.toString())
+                continue
+                
+            if currTrans.name in transMap:
+                for prevTrans in transMap[currTrans.name]:
+                    if currTrans.city != prevTrans.city and abs(currTrans.time - prevTrans.time) <= 60:
+                        res.append(currTrans.toString())
                         break
-
-        return ans
+        return res
+  
         
