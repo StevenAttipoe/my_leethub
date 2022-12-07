0927587558c2495d43beb06c2fc86aed25c4c061
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2022-12-07 14:25:59 +0000

    Time: 1677 ms (26.14%), Space: 18.5 MB (32.84%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,16 +1,21 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        ROW, COL = len(grid), len(grid[0])
+        def islandPerimeterHelper(i, j):
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]):
+                return 1
+            elif grid[i][j] == 0:
+                return 1
+            elif grid[i][j] == -1:
+                return 0
+            else:
+                grid[i][j] = -1
+                
+                return  islandPerimeterHelper(i + 1, j) + islandPerimeterHelper(i - 1, j) + \
+                        islandPerimeterHelper(i, j + 1)  + islandPerimeterHelper(i, j - 1) 
 
-        perimeter = 0
+        ROW, COL = len(grid), len(grid[0])
+        
         for i in range(ROW):
             for j in range(COL):
                 if grid[i][j] == 1:
-                    perimeter +=4
-                    
-                    if i > 0 and grid[i - 1][j] == 1:
-                        perimeter -= 2
-                        
-                    if j > 0 and grid[i][j - 1] == 1:
-                        perimeter -= 2
-        return perimeter
+                    return islandPerimeterHelper(i, j)
