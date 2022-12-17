3550244f9a3b8d53165cd79eb39466ad1802c6f7
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2022-12-17 20:00:40 +0000

    Time: 71 ms (93.06%), Space: 14.7 MB (34.03%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -1,33 +1,38 @@
-class Solution: 
+class Solution:
+    def __init__(self):
+        self.records = {}
+        
+    def populateRecords(self, transactions: List[str]) -> None:
+        for t in transactions:
+            name, time, amount, city = t.split(",")
+            time = int(time)
+            if time in self.records:
+                if name in self.records[time]:
+                    self.records[time][name].append(city)
+                else:
+                    self.records[time][name] = [city]
+            else:
+                self.records[time] = {name:[city]}
+            
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        transactionMap = {}
+        self.populateRecords(transactions)
+        res = []
         for trans in transactions:
             name, time, amount, city = trans.split(",")
             time = int(time)
-            if time in transactionMap:
-                if name in transactionMap[time]:
-                    transactionMap[time][name].append(city)
-                else:
-                    transactionMap[time][name] = [city]
-            else:
-                transactionMap[time] = {name:[city]}
-        
-        invalidTrans = []
-        for trans in transactions:
-            name, time, amount, city = trans.split(",")
             if int(amount) > 1000:
-                invalidTrans.append(trans)
+                res.append(trans)
                 continue
 
-            for time in range(int(time)-60, int(time)+61):
-                if time not in transactionMap:
+            for time in range(time - 60, time + 61):
+                if time not in self.records:
                     continue
-                if name not in transactionMap[time]:
+                if name not in self.records[time]:
                     continue
-                if len(transactionMap[time][name]) > 1 or transactionMap[time][name][0] != city:
-                    invalidTrans.append(trans)
+                if len(self.records[time][name]) > 1 or self.records[time][name][0] != city:
+                    res.append(trans)
                     break
 
-        return invalidTrans
+        return res
 
             
