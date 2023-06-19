15122372e0b60ece346565af46ea12ccd452a660
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-06-19 16:08:59 +0000

    Time: 159 ms (67.27%), Space: 21.1 MB (41.43%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -6,7 +6,7 @@ class Solution:
         intervals = sorted(intervals, key = lambda x:x[0])
         res = [intervals[0]]
 
-        for interval in intervals[1:]:
+        for interval in intervals:
             if interval[0] <= res[-1][1]:
                 res[-1][1] = max(res[-1][1], interval[1])
 
