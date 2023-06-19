c3aaee0ba3c8a55e355fd8e6669244d5bc453d1f
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-06-19 23:49:46 +0000

    Time: 116 ms (62.43%), Space: 16.4 MB (11.68%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,20 +1,23 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row = set()
-        col = set()
+        rows = set()
+        cols = set()
         matrix = set()
         
         for r in range(9):
             for c in range(9):
-                if board[r][c] == ".":
+
+                if board[r][c] == '.':
                     continue
-                    
-                if ((r,board[r][c]) in row or 
-                    (c,board[r][c]) in col or
-                    ((r//3,c//3), board[r][c])  in matrix):
+
+                if ((r, board[r][c]) in rows or
+                    (c, board[r][c]) in cols or
+                    (r // 3, c //3, board[r][c]) in matrix):
                     return False
-                
-                row.add((r, board[r][c]))
-                col.add((c, board[r][c]))
-                matrix.add( ((r//3, c//3), board[r][c]) )
+
+                rows.add((r, board[r][c]))
+                cols.add((c, board[r][c]))
+                matrix.add((r // 3, c // 3, board[r][c]))
+
         return True
+
