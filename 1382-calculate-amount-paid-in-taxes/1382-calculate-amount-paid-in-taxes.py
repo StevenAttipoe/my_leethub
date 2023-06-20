55fc86b9167cf8c7f5dbb0701370f110ce58b9f4
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-06-20 02:12:22 +0000

    Time: 96 ms (26.98%), Space: 16.3 MB (83.45%) - LeetHub

--- 1382-calculate-amount-paid-in-taxes/1382-calculate-amount-paid-in-taxes.py
+++ 1382-calculate-amount-paid-in-taxes/1382-calculate-amount-paid-in-taxes.py
@@ -1,16 +1,18 @@
 class Solution:
     def calculateTax(self, brackets: List[List[int]], income: int) -> float:
         tax = 0
-        initial = 0
+        prevUpper = 0
 
         for upper, percent in brackets:
-            upperDiff = upper - initial
+            #Difference in upper bounds
+            upperDiff = upper - prevUpper
             if income >= 0:
+                #Check if income is less than upperDiff
                 if income < upperDiff: 
                     upperDiff = income
                 tax += (percent / 100) * upperDiff
                 income -= upperDiff
-                initial = upper
+                prevUpper = upper
             else:
                 break
         return tax
