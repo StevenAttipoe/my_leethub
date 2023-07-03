5d85ff328f67ec65f4fd4d26c4680ecd9c760a32
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-07-03 08:23:52 +0000

    Time: 60 ms (31.4%), Space: 16.3 MB (98.4%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,22 +1,18 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         wordSet = set(wordDict)
+        T = [False] * len(s)
+        for i in range(len(s)):
+            for word in wordSet:
+                left = i - len(word) + 1
+                if left < -1:
+                    continue
+                if s[left:i+1] in wordSet and (left == 0 or T[left - 1]):
+                    T[i] = True
+                    break
+        return T[len(s) - 1]
+            
         
-        def wordBreakHelper(word, l, memo):
-            if l == len(word):
-                return True
-            
-            if l in memo:
-                return memo[l]
-            
-            for r in range(l + 1, len(word) + 1):
-                prefix = word[l:r]
-                if prefix in wordSet and wordBreakHelper(word, r, memo):
-                    memo[r] = True
-                    return True
-                memo[l] = False
-            return False
-        return wordBreakHelper(s, 0, {})
-                    
+
         
         
