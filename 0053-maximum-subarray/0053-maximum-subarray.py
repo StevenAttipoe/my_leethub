ff312dd2e59af972f876c8c06e3ad95d6741ebd1
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-07-04 08:14:12 +0000

    Time: 713 ms (60.44%), Space: 30.5 MB (97.05%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
+    def maxSubArray2(self, nums: List[int]) -> int:
         res = nums[0]
         runningSum = 0
         
@@ -11,7 +11,7 @@ class Solution:
                 
         return res
 
-    def maxSubArray2(self, nums):
+    def maxSubArray(self, nums):
         dp = [0]*len(nums)
         for i,num in enumerate(nums):            
             dp[i] = max(dp[i-1] + num, num)
