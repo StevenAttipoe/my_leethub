05fac253e1f52116eb8490b3a35407e3b8deab27
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-08-07 23:05:47 +0000

    Time: 73 ms (97.8%), Space: 16.9 MB (85.92%) - LeetHub

---
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countBattleships(self, board: List[List[str]]) -> int:
+        count = 0
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == 'X':
+                    count += 1
+                    self.dfs(board, i, j)
+        return count
+
+    def dfs(self, board, i, j):
+        if i < 0 or j < 0 or i >= len(board) or j >= len(board[0]) or board[i][j] == '.':
+            return
+
+        board[i][j] = '.'
+
+        self.dfs(board, i + 1, j)
+        self.dfs(board, i - 1, j)
+        self.dfs(board, i, j + 1)
+        self.dfs(board, i, j - 1)
+
+
+        
