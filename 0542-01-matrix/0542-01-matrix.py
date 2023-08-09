44d2bccf1baba96d6b1e1f58419aa3ed0b4a8e08
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-08-09 22:02:03 +0000

    Time: 537 ms (87.28%), Space: 19 MB (95%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,20 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        queue = []
+        neighbours = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                if mat[i][j] == 0:
+                    queue.append((i, j))
+                else:
+                    mat[i][j] = '*'
+
+        for r, c in queue:
+            for dr, dc in neighbours:
+                row = r + dr
+                col = c + dc
+                if 0 <= row < len(mat) and 0 <= col < len(mat[0]) and mat[row][col] == '*':
+                    mat[row][col] = mat[r][c] + 1
+                    queue.append((row, col))
+        return mat
