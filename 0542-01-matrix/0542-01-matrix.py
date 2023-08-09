4a2703003bec7c85b42cc6e3cf206c1cd5defcdb
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-08-09 22:46:00 +0000

    Time: 411 ms (99.94%), Space: 18.9 MB (95%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -18,22 +18,22 @@ class Solution:
 
 				return mat
 			
-    # def updateMatrix2(self, mat: List[List[int]]) -> List[List[int]]:
-    # 		queue = []
-    #     neighbours = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+		def updateMatrix2(self, mat: List[List[int]]) -> List[List[int]]:
+				queue = []
+				neighbours = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 
-    #     for i in range(len(mat)):
-    #         for j in range(len(mat[0])):
-    #             if mat[i][j] == 0:
-    #                 queue.append((i, j))
-    #             else:
-    #                 mat[i][j] = '*'
+				for i in range(len(mat)):
+						for j in range(len(mat[0])):
+								if mat[i][j] == 0:
+										queue.append((i, j))
+								else:
+										mat[i][j] = '*'
 
-    #     for r, c in queue:
-    #         for nr, nc in neighbours:
-    #             row = r + nr
-    #             col = c + nc
-    #             if 0 <= row < len(mat) and 0 <= col < len(mat[0]) and mat[row][col] == '*':
-    #                 mat[row][col] = mat[r][c] + 1
-    #                 queue.append((row, col))
-    #     return mat
+				for r, c in queue:
+						for nr, nc in neighbours:
+								row = r + nr
+								col = c + nc
+								if 0 <= row < len(mat) and 0 <= col < len(mat[0]) and mat[row][col] == '*':
+										mat[row][col] = mat[r][c] + 1
+										queue.append((row, col))
+				return mat
