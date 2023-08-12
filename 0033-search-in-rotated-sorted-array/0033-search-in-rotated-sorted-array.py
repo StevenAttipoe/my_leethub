264a26ddd9da5444b7097eb804f26e6ab0179a9e
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-08-12 10:27:28 +0000

    Time: 44 ms (87.29%), Space: 16.6 MB (70.24%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -13,45 +13,17 @@ class Solution:
 
         pivot = start
         start, end = 0, len(nums) - 1
-        
+
         while start <= end:
             mid = (start + end) // 2
             shifted_mid = (pivot + mid) % len(nums)
+
             if nums[shifted_mid] == target:
                 return shifted_mid
             elif nums[shifted_mid] > target:
                 end = mid - 1
             else:
                 start = mid + 1
+                
         return -1
-
-    # # def search2(self, nums: List[int], target: int) -> int:
-    #     low = 0
-    #     high = len(nums) - 1
-
-    #     while low < high:
-    #         mid = (low + high) // 2
-    #         if nums[mid] > nums[high]:
-    #             low = mid + 1
-    #         else:
-    #             high = mid 
-
-    #     rotated = low
-    #     max_nums = (rotated - 1) % len(nums)
-
-    #     if nums[rotated] <= target <= nums[max_nums]:
-    #         low = 0
-    #         high = len(nums) - 1
-            
-    #         while low <= high:
-    #             mid = (low + high) // 2
-    #             real_mid = (mid + rotated) % len(nums)
-
-    #             if target < nums[real_mid]:
-    #                 high = mid - 1
-    #             elif target > nums[real_mid]:
-    #                 low = mid + 1
-    #             else:
-    #                 return real_mid
-    #     return -1
         
