0ad461fbdf8f8478358ec751181243465f8fc794
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-08-12 10:26:58 +0000

    Time: 56 ms (35.22%), Space: 16.7 MB (35.58%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,37 +1,57 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         pivot = 0
-        # start, end = 0, len(nums)
-        # while start <= end:
-        #     mid = start + (start - end) // 2
-        #     if nums[mid - 1] > nums[mid]:
-        #         pivot = mid
-        #     else:
-        #         left = mid + 1
+        start, end = 0, len(nums) - 1
 
-        for i in range(1, len(nums)):
-            if nums[i - 1] > nums[i]:
-                pivot = i
-                break
-
-        print(pivot)
-        start = end = 0
-        if nums[pivot] <= target <= nums[-1]:
-            start, end = pivot, len(nums)
-        else:
-            start, end = 0, pivot - 1
-
+        #Find pivot
         while start <= end:
-            mid = start + (end - start) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] > target:
+            mid = (end + start) // 2
+            if nums[mid] > nums[-1]:
+                start = mid + 1
+            else:
+                end = mid - 1
+
+        pivot = start
+        start, end = 0, len(nums) - 1
+        
+        while start <= end:
+            mid = (start + end) // 2
+            shifted_mid = (pivot + mid) % len(nums)
+            if nums[shifted_mid] == target:
+                return shifted_mid
+            elif nums[shifted_mid] > target:
                 end = mid - 1
             else:
                 start = mid + 1
         return -1
 
+    # # def search2(self, nums: List[int], target: int) -> int:
+    #     low = 0
+    #     high = len(nums) - 1
 
+    #     while low < high:
+    #         mid = (low + high) // 2
+    #         if nums[mid] > nums[high]:
+    #             low = mid + 1
+    #         else:
+    #             high = mid 
 
-        
+    #     rotated = low
+    #     max_nums = (rotated - 1) % len(nums)
+
+    #     if nums[rotated] <= target <= nums[max_nums]:
+    #         low = 0
+    #         high = len(nums) - 1
+            
+    #         while low <= high:
+    #             mid = (low + high) // 2
+    #             real_mid = (mid + rotated) % len(nums)
+
+    #             if target < nums[real_mid]:
+    #                 high = mid - 1
+    #             elif target > nums[real_mid]:
+    #                 low = mid + 1
+    #             else:
+    #                 return real_mid
+    #     return -1
         
