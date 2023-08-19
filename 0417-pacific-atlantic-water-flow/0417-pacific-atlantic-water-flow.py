5779d519a581a0325232477e85804a6d40d657bb
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-08-19 04:59:45 +0000

    Time: 236 ms (91.9%), Space: 18.3 MB (21.57%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -4,26 +4,28 @@ class Solution:
             return []
 
         n, m = len(heights), len(heights[0])
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         pacific = set()
         atlantic = set()
 
-        def dfs(r, c, visited):
+        def dfs(r, c, visited, prevHeight):
+            if(r < 0 or c < 0 or r >= n or c >= m 
+                or prevHeight > heights[r][c] or (r,c) in visited):
+                return
+
             visited.add((r, c))
-            for dr, dc in directions:
-                i, j = r + dr, c + dc
-                if (0 <= i < n and 0 <= j < m and (i, j) not in visited
-                        and heights[r][c] <= heights[i][j]):
-                    dfs(i, j, visited)
 
-        for r in range(n):
-            dfs(r, 0, pacific)
-            dfs(r, m - 1, atlantic)
-
+            dfs(r + 1, c, visited, heights[r][c])
+            dfs(r - 1, c, visited, heights[r][c])
+            dfs(r, c + 1, visited, heights[r][c])
+            dfs(r, c - 1, visited, heights[r][c])
 
         for c in range(m):
-            dfs(0, c, pacific)
-            dfs(n - 1, c, atlantic)
+            dfs(0, c, pacific, heights[0][c])
+            dfs(n - 1, c, atlantic, heights[n - 1][c])
+
+        for r in range(n):
+            dfs(r, 0, pacific, heights[r][0])
+            dfs(r, m - 1, atlantic, heights[r][m - 1])
 
         return pacific & atlantic
                     
