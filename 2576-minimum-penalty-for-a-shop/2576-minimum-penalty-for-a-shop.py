3cda428212a53539a08f59434a94e9c36071389c
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-08-31 21:34:47 +0000

    Time: 213 ms (26.54%), Space: 33.1 MB (5.02%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.py
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.py
@@ -8,14 +8,11 @@ class Solution:
                 cost += 1
 
         costs = {0 : cost}
-
         for i in range(1, n + 1):
             if customers[i - 1] == 'Y':
                 costs[i] = costs[i - 1] - 1
             else:
                 costs[i] = costs[i - 1] + 1
 
-        minPenalty = min(costs.values())
-        bestTime =[ key for key in costs.keys() if minPenalty == costs[key]]
-        return bestTime[0]
+        return min(costs, key=costs.get)
 
