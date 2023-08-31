9cfc9e87664815abf95f495b09dca252e8386aa2
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-08-31 21:23:18 +0000

    Time: 210 ms (27.4%), Space: 33.2 MB (5.02%) - LeetHub

---
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.py
@@ -0,0 +1,19 @@
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        n = len(customers)
+        cost = 0
+
+        for status in customers:
+            if status == 'Y':
+                cost += 1
+                
+        costs = {0 : cost}
+
+        for i in range(1, n + 1):
+            if customers[i - 1] == 'Y':
+                costs[i] = costs[i - 1] - 1
+            else:
+                costs[i] = costs[i - 1] + 1
+
+        return min(costs, key = costs.get)
+
