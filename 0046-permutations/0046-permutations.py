c9d88efbed45350389059913a82a45906c273945
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-09-02 11:18:59 +0000

    Time: 39 ms (90.79%), Space: 16.6 MB (35.97%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -12,9 +12,27 @@ class Solution:
                     permutation.append(num)
                     helper(permutation)
                     permutation.pop()
-
         helper([])
         
         return permutations
+
+    def permute2(self, nums: List[int]) -> List[List[int]]:
+        permutations = []
+
+        def helper(i):
+            if i == len(nums):
+                permutations.append(nums[:])
+                return
+
+            for j in range(i, len(nums)):
+                nums[i], nums[j] = nums[j], nums[i]
+                helper(i + 1)
+                nums[i], nums[j] = nums[j], nums[i]
+
+        helper(0)
+        
+        return permutations
+
+        
 
         
