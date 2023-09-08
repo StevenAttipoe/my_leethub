d99bfec2ad4d61a77831c076368ae175a2be4f53
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-09-08 09:34:49 +0000

    Time: 41 ms (45.95%), Space: 16.2 MB (90.82%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,5 +1,5 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
+    def climbStairs2(self, n: int) -> int:
         dp = [0] * (n + 1)
         dp[0] = dp[1] = 1
 
@@ -7,3 +7,18 @@ class Solution:
             dp[i] = dp[i - 1] + dp[i - 2]
         
         return dp[-1]
+
+    def climbStairs(self, n: int) -> int:
+        memo = [0] * (n + 1)
+        return self.takeSteps(0, n, memo)
+    
+    def takeSteps(self, step, n, memo):
+        if step > n:
+            return 0
+        if step == n:
+            return 1
+        if memo[step] > 0:
+            return memo[step]
+
+        memo[step] = self.takeSteps(step + 1, n, memo) + self.takeSteps(step + 2, n, memo)
+        return memo[step]
