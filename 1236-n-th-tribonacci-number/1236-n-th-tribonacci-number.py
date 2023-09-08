77725c90cfd355a085df33d7f5ef1e7f24e93943
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-09-08 09:52:18 +0000

    Time: 31 ms (93.31%), Space: 16.2 MB (65.79%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -5,6 +5,20 @@ class Solution:
         if n == 1 or n == 2:
             return 1
 
+        a, b, c = 0, 1, 1
+
+        for i in range(3, n + 1):
+            d = a + b + c
+            a, b, c = b, c, d
+
+        return c
+
+    def tribonacci2(self, n: int) -> int:
+        if n == 0:
+            return n
+        if n == 1 or n == 2:
+            return 1
+
         dp = [0] * (n + 1)
         dp[1] = dp[2] = 1
 
