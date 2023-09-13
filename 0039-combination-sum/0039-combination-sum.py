cd7b8eb07f31d6973a19fcdd917e47e99a6e9601
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-09-13 15:46:49 +0000

    Time: 53 ms (88.61%), Space: 16.5 MB (53.55%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -19,4 +19,17 @@ class Solution:
         backtrack(target, [], 0)
         return results
 
+    # Time Complexity: O(M*M*N), N = len(candidates), M = target
+    # Space Complexity: O(M*M)
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        dp = [[] for _ in range(target+1)]
+        for c in candidates: # O(N): for each candidate
+            for i in range(c, target+1): # O(M): for each possible value <= target
+                if i == c: 
+                    dp[i].append([c])
+                for comb in dp[i-c]: 
+                    dp[i].append(comb + [c]) # O(M) worst: for each combination
+        return dp[-1]
+
+
         
