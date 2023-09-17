fb9355cc4eeed80006de051951c9bf26a8e4c482
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-09-17 08:11:28 +0000

    Time: 36 ms (95%), Space: 16.5 MB (66.16%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -35,6 +35,24 @@ class Solution:
         
         return permutations
 
+    def permute3(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        count = Counter(nums)
+
+        def helper(cur):
+            if len(cur) == len(nums):
+                res.append(cur)
+                return
+
+            for num in count.keys():
+                if count[num]:
+                    count[num] -= 1
+                    helper(cur + [num])
+                    count[num] += 1
+
+        helper([])
+        return res
+
         
 
         
