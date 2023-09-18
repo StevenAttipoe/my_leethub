0344baa232bba263484cacecd1a410289864655f
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-09-18 10:20:54 +0000

    Time: 31 ms (97.56%), Space: 16.3 MB (80.26%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,5 +1,16 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
+        charSum = 0
+        for char in t:
+            charSum += ord(char)
+
+        for char in s:
+            charSum -= ord(char)
+
+        return chr(charSum)
+
+
+    def findTheDifference2(self, s: str, t: str) -> str:
         for char in t:
             if t.count(char) != s.count(char):
                 return char
