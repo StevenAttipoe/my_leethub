2f2ecbf37e076132d49c479c885a425c260ae368
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-09-24 18:47:42 +0000

    Time: 249 ms (79.06%), Space: 20.9 MB (28.34%) - LeetHub

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/1055-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/1055-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -1,12 +1,25 @@
 class Solution:
-    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+    def numPairsDivisibleBy60III(self, time: List[int]) -> int:
         noOfPairs = 0
         count = [0] * 60
         
         for duration in time:
             remainder, complement = duration % 60, -duration % 60
+            print(remainder, complement)
             noOfPairs += count[complement]
             count[remainder] += 1
+
+        return noOfPairs
+
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        noOfPairs = 0
+        count = {}
+        
+        for duration in time:
+            remainder, complement = duration % 60, (60-duration) % 60
+            if complement in count:
+                noOfPairs += count[complement]
+            count[remainder] = count.get(remainder, 0) + 1
 
         return noOfPairs
 
