349d1a063d1bc45a52bdf51130402ad5e256f601
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-09-25 17:20:17 +0000

    Time: 814 ms (8.81%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,13 +1,14 @@
-# SELECT max(salary) as SecondHighestSalary
-# from Employee
-# WHERE salary < (SELECT max(salary) from Employee);
-
 # ORDER BY value, LIMIT n-1 OFFSET n
 SELECT IFNULL(
   NULL, (
     SELECT DISTINCT salary
     FROM Employee
     ORDER BY Salary DESC
-    LIMIT 1, 1
+    LIMIT 1 OFFSET 1
   )
 ) AS SecondHighestSalary;
+
+
+# SELECT max(salary) as SecondHighestSalary
+# from Employee
+# WHERE salary < (SELECT max(salary) from Employee);
