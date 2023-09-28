ecac64fe9932c09acc47cd31b73d773a3f71de8e
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-09-28 19:54:36 +0000

    Time: 9522 ms (5.01%), Space: 18.6 MB (68.58%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,65 +1,64 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
 class Trie:
     def __init__(self):
-        self.root = {}
-    
+        self.root = TrieNode()
+
     def insert(self, word):
-        t = self.root
-        for c in word:
-            if c not in t:
-                t[c] = {}
-            t = t[c]
-        
-        t["-"] = True
+        cur = self.root
+
+        for char in word:
+            if char not in cur.children:
+                cur.children[char] = TrieNode()
+            cur = cur.children[char]
+        cur.isEnd = True
 
 class Solution:
-    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        rowNum = len(board)
-        colNum = len(board[0])
-        result = []
-        trie = Trie()
-        
+    def __init__(self):
+        self.trie = Trie()
+
+    def buildTrie(self, words):
         for word in words:
-            trie.insert(word)
+            self.trie.insert(word)
+
+    def search(self, r, c, board, res, trie, word):
+        if trie.isEnd:
+            res.append(''.join(word))
+            trie.isEnd = False
+
+        if r < 0 or r >= len(board):
+            return
+
+        if c < 0 or c >= len(board[0]):
+            return
+
+        if board[r][c] == '#' or board[r][c] not in trie.children:
+            return
+
+        char = board[r][c]
+        board[r][c] = '#'
+        trie = trie.children[char]
+
+        self.search(r + 1, c, board, res, trie, word + [char])
+        self.search(r - 1, c, board, res, trie, word + [char])
+        self.search(r, c + 1, board, res, trie, word + [char])
+        self.search(r, c - 1, board, res, trie, word + [char])
+        board[r][c] = char
         
-        def dfs(row, col, path, parent):
-            nonlocal rowNum
-            nonlocal colNum
-            
-            if '-' in parent:
-                result.append(path)
-                # avoid duplicate
-                del parent['-']
-            
-            if row < 0 or row >= rowNum or col < 0 or col >= colNum:
-                return
-            
-            char = board[row][col]
-            
-            if char == "#":
-                return
-            
-            if char not in parent:
-                return
-            
-            curNode = parent[char]
-            
-            # find the char, set to visited
-            board[row][col] = "#"
-            dfs(row+1, col, path + char, curNode)
-            dfs(row-1, col, path + char, curNode)
-            dfs(row, col+1, path + char, curNode)
-            dfs(row, col-1, path + char, curNode)
-            board[row][col] = char
-            
-            # optimization: remove the matched leaf node in Trie.
-            if not curNode:
-                parent.pop(char)
-            
-        
-        for row in range(rowNum):
-            for col in range(colNum):
-				# optimiztation 
-                if board[row][col] in trie.root:
-                    dfs(row, col, "", trie.root)
-        
-        return result
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        self.buildTrie(words)
+
+        ROW, COL = len(board), len(board[0])
+        foundWords = []
+
+        for r in range(ROW):
+            for c in range(COL):
+                if board[r][c] in self.trie.root.children:
+                    self.search(r, c, board, foundWords, self.trie.root, [])
+
+        return foundWords
+
+
