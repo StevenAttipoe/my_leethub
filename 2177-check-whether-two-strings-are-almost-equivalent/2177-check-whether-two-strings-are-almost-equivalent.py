4c0ee48bd84a8c981b362920db34d7ca049125f5
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-10-11 11:54:28 +0000

    Time: 27 ms (98.62%), Space: 16.3 MB (58.35%) - LeetHub

--- 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.py
+++ 2177-check-whether-two-strings-are-almost-equivalent/2177-check-whether-two-strings-are-almost-equivalent.py
@@ -1,5 +1,18 @@
 class Solution:
     def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
+        count = [0] * 26
+
+        for i in range(len(word1)):
+            count[ord(word1[i]) - ord('a')] += 1
+            count[ord(word2[i]) - ord('a')] -= 1
+
+        for i in range(26):
+            if abs(count[i]) > 3:
+                return False
+
+        return True
+
+    def checkAlmostEquivalent2(self, word1: str, word2: str) -> bool:
         count1 = Counter(word1)
         count2 = Counter(word2)
 
