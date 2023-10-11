84b631702733c2e3645282538dbf45e294c1e160
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-10-11 11:26:03 +0000

    Time: 154 ms (46.33%), Space: 16.8 MB (30.06%) - LeetHub

--- 1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.py
+++ 1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.py
@@ -1,5 +1,17 @@
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        boxTypes = sorted(boxTypes, key = lambda x:x[1])
+        maxUnits = 0
+
+        while truckSize > 0 and boxTypes:
+            boxCount, unitCount = boxTypes.pop()
+            boxCount = min(boxCount, truckSize)
+            truckSize -= boxCount
+            maxUnits += unitCount * boxCount
+
+        return maxUnits
+
+    def maximumUnits1(self, boxTypes: List[List[int]], truckSize: int) -> int:
         heap = []
         for boxCount, unitCount in boxTypes:
             heapq.heappush(heap, [-unitCount, boxCount])
