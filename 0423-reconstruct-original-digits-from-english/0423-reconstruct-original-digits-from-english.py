8c456d9d6f5b824c8166e3ed99797060281f3917
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-10-11 10:15:20 +0000

    Time: 48 ms (84.84%), Space: 16.6 MB (89.63%) - LeetHub

--- 0423-reconstruct-original-digits-from-english/0423-reconstruct-original-digits-from-english.py
+++ 0423-reconstruct-original-digits-from-english/0423-reconstruct-original-digits-from-english.py
@@ -1,5 +1,26 @@
 class Solution:
     def originalDigits(self, s: str) -> str:
+        count = Counter(s)
+        nums_map = {}
+
+        nums_map[0] = count.get('z', 0)
+        nums_map[2] = count.get('w', 0)
+        nums_map[4] = count.get('u', 0)
+        nums_map[6] = count.get('x', 0)
+        nums_map[8] = count.get('g', 0)
+        nums_map[1] = count.get('o', 0) - nums_map[0] - nums_map[2] - nums_map[4]
+        nums_map[3] = count.get('t', 0) - nums_map[2] - nums_map[8]
+        nums_map[5] = count.get('f', 0) - nums_map[4]
+        nums_map[7] = count.get('s', 0) - nums_map[6]
+        nums_map[9] = count.get('i', 0) - nums_map[5] - nums_map[6] - nums_map[8]
+
+        digits = []
+        for digit, freq in nums_map.items():
+            digits.append(str(digit) * freq)
+
+        return ''.join(sorted(digits))
+
+    def originalDigits2(self, s: str) -> str:
         nums_map = {
             'w': ('two', '2'),
             'u': ('four', '4'),
