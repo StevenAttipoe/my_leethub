edbb802e26f53a597ae77549d76c89b3fd61533c
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-10-19 13:44:44 +0000

    Time: 63 ms (64.55%), Space: 30.2 MB (16.2%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,5 +1,18 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        dna_map = {}
+        res = []
+
+        for i in range(len(s) - 9):
+            dna = s[i:i + 10]
+            dna_map[dna] = dna_map.get(dna, 0) + 1
+            if dna_map[dna] == 2:
+                res.append(dna)
+
+        return res
+
+
+    def findRepeatedDnaSequences2(self, s: str) -> List[str]:
         if len(s) < 10:
             return []
 
