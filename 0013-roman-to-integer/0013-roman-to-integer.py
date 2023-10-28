1ddbab220356b2aa3ae6fda5d551e90bf4331de4
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-10-28 18:36:59 +0000

    Time: 58 ms (24.13%), Space: 16.2 MB (56.91%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,28 +1,20 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        symbols = {
-            'I':1,
-            'V':5,
-            'X':10,
-            'L':50,
-            'C':100,
-            'D':500,
-            'M':1000
-        }
+        SYMBOLS = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
 
-        prev, cur = symbols[s[0]], 0
-        integer = 0
-        for char in s[1:]:
-            cur = symbols[char]
-            if prev < cur:
-                integer -= prev
+        num = i = 0
+        while i < len(s) - 1:
+            if SYMBOLS[s[i]] < SYMBOLS[s[i + 1]]:
+                num += SYMBOLS[s[i + 1]] - SYMBOLS[s[i]]
+                i += 2
             else:
-                integer += prev
-            prev = cur
-        integer += prev
+                num += SYMBOLS[s[i]]
+                i += 1
 
-        return integer
-        
+        if i < len(s):
+            num += SYMBOLS[s[i]]
+
+        return num
 
 
         
