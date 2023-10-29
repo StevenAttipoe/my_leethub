2c42917a3459d1997ffac6e1a009606af0d19d6c
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-10-29 09:11:24 +0000

    Time: 230 ms (49.65%), Space: 20.3 MB (38.94%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.py
+++ 0817-design-hashmap/0817-design-hashmap.py
@@ -1,17 +1,55 @@
-class MyHashMap:
-
+class Bucket:
     def __init__(self):
-        self.data = [None] * 1000001        
+        self.bucket = []
+
+    def put(self, key, value):
+        for i, (k, _) in enumerate(self.bucket):
+            if k == key:
+                self.bucket[i] = (key, value)
+                return
+        self.bucket.append((key, value))
+    
+    def get(self, key):
+        for k, v in self.bucket:
+            if k == key:
+                return v
+        return -1
+    
+    def remove(self, key):
+        for i, (k, _) in enumerate(self.bucket):
+            if k == key:
+                del self.bucket[i]
+
+class MyHashMap:
+    def __init__(self):
+        self.key_space = 2069
+        self.hash_table = [Bucket() for _ in range(self.key_space)]
 
     def put(self, key: int, value: int) -> None:
-        self.data[key] = value        
+        hash_key = key % self.key_space
+        self.hash_table[hash_key].put(key, value)
 
     def get(self, key: int) -> int:
-        val = self.data[key]
-        return val if val != None else -1
+        hash_key = key % self.key_space
+        return self.hash_table[hash_key].get(key)
 
     def remove(self, key: int) -> None:
-        self.data[key] = None
+        hash_key = key % self.key_space
+        return self.hash_table[hash_key].remove(key)
+
+# class MyHashMap2:
+#     def __init__(self):
+#         self.data = [None] * 1000001        
+
+#     def put(self, key: int, value: int) -> None:
+#         self.data[key] = value        
+
+#     def get(self, key: int) -> int:
+#         val = self.data[key]
+#         return val if val != None else -1
+
+#     def remove(self, key: int) -> None:
+#         self.data[key] = None
 
         
 
