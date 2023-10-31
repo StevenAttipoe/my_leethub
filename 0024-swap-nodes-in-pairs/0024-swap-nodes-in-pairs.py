755ac451dbbcd2b35c97d295b0302dbdd8ad34aa
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-10-31 10:35:49 +0000

    Time: 37 ms (71.5%), Space: 16.2 MB (50.3%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -8,22 +8,28 @@ class Solution:
         if not head or not head.next:
             return head
 
-        new_head = head.next
-        prev = None
         cur = head
-
+        newHead = head.next
+        prev = None
         while cur and cur.next:
-            a = cur
-            b = cur.next
-            c = cur.next.next
-
+            a, b = cur, cur.next
             if prev:
                 prev.next = b
+            b.next, a.next = a, b.next
+            prev = a 
+            cur = cur.next
 
-            b.next = a
-            a.next = c
+        return newHead
+            # 1 -- 2 -- 3 -- 4
+            # 2 -- 1 -- 3 -- 4
+            # 2 -- 1 -- 4 -- 3
 
-            prev = a
-            cur = c
-        return new_head            
 
+            # 0 -- 1 -- 2 -- 3 -- 4 -- 5
+            # 0 -- 2 -- 1 -- 3 -- 4 -- 5
+            # 0 -- 2 -- 1 -- 4 -- 3 -- 5
+
+            # 0 -- 1 -- 2 -- 3 -- 4 -- 5 -- 6
+        
+
+        
