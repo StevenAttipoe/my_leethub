e1f517b4f058a36c7d8cf8287760b6c35a2dae96
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-11-01 17:26:23 +0000

    Time: 751 ms (33.82%), Space: 81.3 MB (55.36%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.py
+++ 0460-lfu-cache/0460-lfu-cache.py
@@ -58,7 +58,7 @@ class LFUCache:
                 dll = self.frequencies[self.minf]
                 del self.cache[dll.tail.prev.key]
                 dll.removeNode(dll.tail.prev)
-                if dll.head.next == dll.tail:
+                if dll.head.next == dll.tail: #optimization
                     del self.frequencies[self.minf]
 
             newNode = Node(key, value)
@@ -66,7 +66,49 @@ class LFUCache:
             self.frequencies[1].addToHead(newNode)
             self.minf = 1
         
+class LFUCache2:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.frequencies = defaultdict(OrderedDict)
+        self.minf = 0
 
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        
+        freq, val = self.cache[key]
+        del self.frequencies[freq][key]
+        self.cache[key] = (freq + 1, val)
+        self.frequencies[freq + 1][key] = True
+        if self.minf == freq and not self.frequencies[self.minf]:
+            self.minf += 1
+
+        return val
+
+    def put(self, key: int, value: int) -> None:
+        if self.capacity == 0:
+            return
+
+        if key in self.cache:
+            freq, val = self.cache[key]
+            self.cache[key] = (freq, value)
+            self.get(key)
+        else:
+            if len(self.cache) >= self.capacity:
+                k, _ = self.frequencies[self.minf].popitem(last= False)
+                del self.cache[k]
+
+            self.cache[key] = (1, value)
+            self.frequencies[1][key] = True
+            self.minf = 1
+
+
+
+# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
 
 # Your LFUCache object will be instantiated and called as such:
 # obj = LFUCache(capacity)
