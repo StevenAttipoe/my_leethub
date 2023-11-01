54d1341bf5d0175c041e9f78eefdfd67bd4e584f
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-11-01 21:11:54 +0000

    Time: 77 ms (77.49%), Space: 16.4 MB (27.38%) - LeetHub

--- 0815-champagne-tower/0815-champagne-tower.py
+++ 0815-champagne-tower/0815-champagne-tower.py
@@ -1,16 +1,16 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        towers = [[0] * i for i in range(1, query_row + 2)]
-        towers[0][0] = poured
+        tower = [[0] * i for i in range(1, query_row + 2)]
+        tower[0][0] = poured
 
         for i in range(query_row):
-            for j in range(len(towers[i])):
-                excess = (towers[i][j] - 1) / 2
+            for j in range(len(tower[i])):
+                excess = (tower[i][j] - 1) / 2
                 if excess > 0:
-                    towers[i + 1][j] += excess
-                    towers[i + 1][j + 1] += excess
+                    tower[i + 1][j] += excess
+                    tower[i + 1][j + 1] += excess
 
-        return min(1, towers[query_row][query_glass])
+        return min(1, tower[query_row][query_glass])
         
 
         
