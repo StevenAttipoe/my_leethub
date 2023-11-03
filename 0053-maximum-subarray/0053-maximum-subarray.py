c9e543bcd83e6a0961308b4716db31a6ea832373
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-11-03 18:38:58 +0000

    Time: 587 ms (73.58%), Space: 30.4 MB (86.23%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,25 +1,5 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        current_subarray = max_subarray = nums[0]
-        
-        for num in nums[1:]:
-            current_subarray = max(num, current_subarray + num)
-            max_subarray = max(max_subarray, current_subarray)
-        
-        return max_subarray
-
-    def maxSubArray2(self, nums):
-        n = len(nums)
-        dp = [0] * n
-        dp[0] = nums[0]
-
-        for i in range(1, n): 
-            num = nums[i]           
-            dp[i] = max(dp[i - 1] + num, num)
-
-        return max(dp)
-
-    def maxSubArray3(self, nums: List[int]) -> int:
         res = nums[0]
         runningSum = 0
         
@@ -30,4 +10,36 @@ class Solution:
                 runningSum = 0
                 
         return res
+
+    def maxSubArray2(self, nums: List[int]) -> int:
+        current_subarray = max_subarray = nums[0]
+        
+        for num in nums[1:]:
+            current_subarray = max(num, current_subarray + num)
+            max_subarray = max(max_subarray, current_subarray)
+        
+        return max_subarray
+
+    def maxSubArray3(self, nums):
+        n = len(nums)
+        dp = [0] * n
+        dp[0] = nums[0]
+
+        for i in range(1, n): 
+            num = nums[i]           
+            dp[i] = max(dp[i - 1] + num, num)
+
+        return max(dp)
+
+    def maxSubArray4(self, nums: List[int]) -> int: # O(n ^ n) tim
+        res = -math.inf
+
+        for i in range(len(nums)):
+            cur_subarray = 0
+            for j in range(i, len(nums)):
+                cur_subarray += nums[j]
+                res = max(res, cur_subarray)
+
+        return res
+    
         
