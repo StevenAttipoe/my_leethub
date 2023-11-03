72d741ea396f7e95657c59d0e1ff9b559aa797bb
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-11-03 09:11:32 +0000

    Time: 79 ms (78.28%), Space: 18.5 MB (44.24%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -1,5 +1,43 @@
 class Solution:
-    def deleteAndEarn(self, nums: List[int]) -> int:
+    def deleteAndEarn(self, nums: List[int]) -> int: # O(N log N) time & O(N) space
+        points = defaultdict(int)
+
+        for n in nums:
+            points[n] += n
+
+        sorted_nums = sorted(points.keys())
+        first = 0
+        second = points[sorted_nums[0]]
+
+        for i in range(1, len(sorted_nums)):
+            cur = sorted_nums[i]
+
+            if cur == sorted_nums[i - 1] + 1:
+                first, second = second, max(second, first + points[cur])
+            else:
+                first, second = second, second + points[cur]
+
+        return second
+
+    def deleteAndEarn2(self, nums: List[int]) -> int: # O(N + K) time & O(N) space
+        points = {}
+        high = 0
+
+        for n in nums:
+            points[n] = points.get(n, 0) + n
+            high = max(high, n)
+
+        first = 0
+        second = points.get(1, 0)
+
+        for i in range(2, high + 1):
+            cur = max(second, first + points.get(i, 0))
+            first = second
+            second = cur
+
+        return second
+
+    def deleteAndEarn3(self, nums: List[int]) -> int: # O(N + K) time & O(N + K) space
         points = {}
         high = 0
 
