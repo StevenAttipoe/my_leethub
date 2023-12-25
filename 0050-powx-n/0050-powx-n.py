7705b1063ad1d0fd389972f1b750f728f1e49256
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-12-25 17:49:36 +0000

    Time: 38 ms (60.41%), Space: 17.3 MB (5.61%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,33 @@
+class Solution:
+    # O(logn)
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n == 1:
+            return x
+        if n < 0:
+            x = 1 / x
+            n = abs(n)
+
+        half = self.myPow(x, n // 2)
+        if n % 2 == 0:
+            return half * half
+        else:
+            return half * half * x
+
+    # O(n)
+    def myPow2(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n == 1:
+            return x
+        if n < 0:
+            x = 1 / x
+            n = abs(n)
+
+        res = 1
+        while n > 0:
+            res *= x
+            n -= 1
+
+        return res
