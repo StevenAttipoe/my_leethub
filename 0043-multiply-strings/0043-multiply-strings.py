65de55747aa744045c8f3f542a6d84b3a7661f63
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2023-12-26 23:51:27 +0000

    Time: 96 ms (41%), Space: 17.5 MB (7.49%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,5 +1,18 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
+        res = 0
+        carry1 = carry2 = 1
+
+        for i in num2[::-1]:
+            carry1 = 1
+            for j in num1[::-1]:
+                res += int(i) * int(j) * carry1 * carry2
+                carry1 *= 10
+            carry2 *= 10
+
+        return str(res)
+
+    def multiply2(self, num1: str, num2: str) -> str:
         carry = power = 0
         res = []
 
