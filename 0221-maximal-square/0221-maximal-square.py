1738fd820c7419f72aba14cabb950fafb1da146d
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2024-01-08 01:56:41 +0000

    Time: 759 ms (25.83%), Space: 63.2 MB (5.63%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,31 +1,33 @@
 class Solution:
+    """
+        Time - O(n * m)
+        Space - O(n * m)
+    """
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         ROWS, COLS = len(matrix), len(matrix[0])
-        maxlength = 0
         memo = {}
 
-        for i in range(ROWS):
-            for j in range(COLS):
-                if matrix[i][j] == '1':
-                    length = self.recurseSquaresMemo(matrix, i, j, memo)
-                    maxlength = max(maxlength, length)
+        def helper(i, j):
+            if i >= len(matrix) or j >= len(matrix[0]):
+                return 0
 
-        return maxlength * maxlength
+            if (i, j) in memo:
+                return memo[(i, j)]
 
-    def recurseSquaresMemo(self, matrix, i, j, memo):
-        if i == len(matrix) or j == len(matrix[0]):
-            return 0
+            right = helper(i, j + 1)
+            down = helper(i + 1, j)
+            diag = helper(i + 1, j + 1)
 
-        if (i, j) in memo:
+            memo[(i, j)] = 0
+            if matrix[i][j] == '1':
+                memo[(i, j)] = 1 + min(right, down, diag)
+
             return memo[(i, j)]
 
-        right = self.recurseSquaresMemo(matrix, i, j + 1, memo)
-        bottom = self.recurseSquaresMemo(matrix, i + 1, j, memo)
-        diagonal = self.recurseSquaresMemo(matrix, i + 1, j + 1, memo)
+        helper(0, 0)
+        return (max(memo.values())) ** 2
 
-        res = 1 + min(right, bottom, diagonal) if matrix[i][j] == '1' else 0
-        memo[(i, j)] = res
-        return memo[(i, j)]
+
 
     # Time - O(n * m)^ 2
     def maximalSquare2(self, matrix: List[List[str]]) -> int:
