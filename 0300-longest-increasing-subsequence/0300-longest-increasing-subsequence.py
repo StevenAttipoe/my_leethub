53f696bfc3a3c9905259eb6e9e23d0e6fe3a114e
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2024-01-09 04:06:59 +0000

    Time: 67 ms (86.6%), Space: 17.9 MB (10.81%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,6 +1,35 @@
 class Solution:
-    #O(n ^ 2) time
+    # O(n * log n)
     def lengthOfLIS(self, nums: List[int]) -> int:
+        LIS = []
+
+        for n in nums:
+            i = bisect_left(LIS, n)
+
+            if i == len(LIS):
+                LIS.append(n)
+            else:
+                LIS[i] = n
+        return len(LIS)
+
+    #O(n ^ 2) time
+    def lengthOfLIS2(self, nums: List[int]) -> int:
+        LIS = [nums[0]]
+
+        for n in nums[1:]:
+            if n > LIS[-1]:
+                LIS.append(n)
+            else:
+                i = 0
+                while LIS[i] < n: 
+                    i += 1
+                LIS[i] = n
+            print(LIS)
+        return len(LIS)
+
+
+    #O(n ^ 2) time
+    def lengthOfLIS2(self, nums: List[int]) -> int:
         LEN = len(nums)
         LIS = [1] * LEN
 
@@ -10,5 +39,4 @@ class Solution:
                     LIS[i] = max(LIS[i], 1 + LIS[j])
 
         return max(LIS)
-                
         
