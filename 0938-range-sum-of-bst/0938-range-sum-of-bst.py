5943af291d0d194b598be2a7b5f7988d74a8c085
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2024-01-23 09:57:25 +0000

    Time: 132 ms (42.17%), Space: 23.6 MB (99.44%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.py
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -6,6 +6,21 @@
 #         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        def recurseTree(node):
+            if node:
+                nonlocal rangeSum
+
+                if low <= node.val <= high:
+                    rangeSum += node.val
+
+                recurseTree(node.left)
+                recurseTree(node.right)
+
+        rangeSum = 0
+        recurseTree(root) 
+        return rangeSum
+
+    def rangeSumBST2(self, root: Optional[TreeNode], low: int, high: int) -> int:
         rangeSum = [0]
 
         def recurseTree(node):
