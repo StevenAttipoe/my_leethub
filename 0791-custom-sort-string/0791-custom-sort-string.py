fd2622fe8b3c50a43a18c8b26c4591e4dcaa8626
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2024-01-24 12:04:33 +0000

    Time: 34 ms (80.03%), Space: 16.5 MB (58.95%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -1,5 +1,22 @@
 class Solution:
+    # O(n) runtime & O(n) space
     def customSortString(self, order: str, s: str) -> str:
+        charFreq = Counter(s)
+        res = []
+
+        for char in order:
+            if char in charFreq:
+                res.append(char * charFreq[char])
+                charFreq[char] = 0
+
+        for char, freq in charFreq.items():
+            res.append(char * freq)
+        
+        return ''.join(res)
+
+
+    # O(nlogn) runtime & O(n) space
+    def customSortString2(self, order: str, s: str) -> str:
         rank = [26] * 26
 
         for i, char in enumerate(order):
