d9cfc992fd90bff117d1c7d092e3b65443929b5a
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2024-01-29 11:28:16 +0000

    Time: 161 ms (69.05%), Space: 21.4 MB (77.49%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -1,5 +1,4 @@
 class Solution:
-
     def __init__(self, w: List[int]):
         self.runningSums = []
         self.cumulativeSum = 0
@@ -9,10 +8,38 @@ class Solution:
             self.runningSums.append(self.cumulativeSum)
 
     def pickIndex(self) -> int:
-        randomWeight = self.cumulativeSum * random.random()
+        target = random.uniform(0, self.cumulativeSum)
+        return self.binarySearch(self.runningSums, target)
+
+    
+    def binarySearch(self, runningSums, target) -> int:
+        l, r = 0, len(runningSums)
+
+        while l < r:
+            mid = (l + r) // 2
+
+            if runningSums[mid] < target:
+                l = mid + 1
+            else:
+                #the search space greater than mid mid but we want the min greatest value
+                r = mid
+        #return l or r, it doesn't matter
+        return r
+
+class Solution2:
+    def __init__(self, w: List[int]):
+        self.runningSums = []
+        self.cumulativeSum = 0
+
+        for weight in w:
+            self.cumulativeSum += weight
+            self.runningSums.append(self.cumulativeSum)
+
+    def pickIndex(self) -> int:
+        target = random.uniform(0, self.cumulativeSum)
 
         for index, runningSum in enumerate(self.runningSums):
-            if runningSum > randomWeight:
+            if runningSum > target:
                 return index
 
 
