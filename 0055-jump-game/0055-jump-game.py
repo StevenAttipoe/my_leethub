a5a1d8731fe1c8be47cf04adff5ccf734851a453
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2024-02-25 23:21:49 +0000

    Time: 352 ms (75.36%), Space: 17.8 MB (74.1%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,10 +1,43 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        nextToLast = len(nums) - 1
-        currentStep = nextToLast
+        gas = 0
 
-        for index in range(len(nums) - 2, -1, -1):
-            if index + nums[index] >= currentStep:
-                currentStep = index
+        for n in nums:
+            if gas < 0:
+                return False
 
-        return currentStep == 0
+            elif n > gas:
+                gas = n
+            
+            gas -= 1
+
+        return True
+
+    def canJump2(self, nums: List[int]) -> bool:
+        lastIndex = len(nums) - 1
+
+        for curIndex in range(len(nums) - 2, -1, -1):
+            jumpStep = nums[curIndex]
+            if curIndex + jumpStep >= lastIndex:
+                lastIndex = curIndex
+
+        return lastIndex == 0
+
+    def canJump3(self, nums: List[int]) -> bool:
+        N = len(nums)
+        dp = [False] * N
+        dp[0] = True
+
+        for i in range(N):
+            if dp[i]:
+                jumpStep = nums[i]
+                for j in range(1, jumpStep + 1):
+                    #Set reachable index to True
+                    if i + j < N:
+                        dp[i + j] = True
+
+                    #If can jump to last index
+                    if i + j == N - 1:
+                        return True
+
+        return dp[-1]
