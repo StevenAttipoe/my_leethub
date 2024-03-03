c8e793e3ccf919e77b9d69d3ae24836d38338569
Steven Attipoe <60293933+StevenAttipoe@users.noreply.github.com>
2024-03-03 07:07:39 +0000

    Time: 40 ms (55.94%), Space: 16.6 MB (69.91%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,26 +1,31 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
+        if len(s) < 4 or len(s) > 12: 
+            return []
+
         N = len(s)
-
-        if N < 4: return []
-
         validIPs = []
 
-        for i in range(N):
-            for j in range(i + 1, N):
-                for k in range(j + 1, N - 1):
-                    if i < 3 and j - i < 4 and k - j < 4 and 0 < N - k <= 4:
-                        a, b, c, d = s[:i + 1], s[i + 1:j + 1], s[j + 1:k + 1], s[k + 1: N]
-                        if (
-                            (len(a) > 1 and a[0] == '0' or int(a) > 255) or 
-                            (len(b) > 1 and b[0] == '0' or int(b) > 255) or
-                            (len(c) > 1 and c[0] == '0' or int(c) > 255) or
-                            (len(d) > 1 and d[0] == '0' or int(d) > 255) 
-                        ): continue
+        for i in range(1, min(4, N - 2)):
+            for j in range(i + 1, min(i + 4, N - 1)):
+                for k in range(j + 1, min(j +4, N)):
+                    a, b, c, d = s[:i], s[i:j], s[j:k], s[k:]
+                    if (
+                        self.isNotValid(a) or 
+                        self.isNotValid(b) or
+                        self.isNotValid(c) or
+                        self.isNotValid(d)
+                    ) : continue
 
-                        IP = a + '.' + b + '.' + c + '.' + d
-                        validIPs.append(IP)
+                    IP = a + '.' + b + '.' + c + '.' + d
+                    validIPs.append(IP)
 
         return validIPs
+
+    def isNotValid(self, segment):
+        if len(segment) > 1 and segment[0] == '0' or int(segment) > 255:
+            return True
+        return False
+    
 
         
