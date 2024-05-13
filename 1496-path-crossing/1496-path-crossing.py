0aad4b14f75c9999d37e5eb3bd1b72dcf1bb9749
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2024-05-13 14:29:02 +0000

    Time: 34 ms (69.73%), Space: 16.6 MB (44.31%) - LeetHub

---
+++ 1496-path-crossing/1496-path-crossing.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        CARDINALS = {
+            'N' : (0, 1),
+            'S' : (0, -1),
+            'W' : (-1, 0),
+            'E' : (1, 0)
+        }
+        
+        visited = {(0,0)}
+        x = y = 0
+
+        for c in path:
+            dx, dy = CARDINALS[c]
+            x += dx
+            y += dy
+
+            if (x,y) in visited:
+                return True
+
+            visited.add((x,y))
+
+        return False
+
+
+        
