cc0cbf42ea03939e6ab0f5966ce45195d866373f
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2024-09-04 00:27:21 +0000

    Time: 35 ms (56.87%), Space: 16.6 MB (20.81%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -4,21 +4,12 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
-            return head
-
-        newHead = head.next
-        head.next = self.swapPairs(head.next.next)
-        newHead.next = head
-
-        return newHead
-
     def swapPairs2(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return head
-
-        cur, newHead = head, head.next
+        
+        newHead = head.next
+        cur = head
         prev = None
 
         while cur and cur.next:
@@ -27,18 +18,23 @@ class Solution:
             if prev:
                 prev.next = b
 
-            self.swap(a, b, c)
+            b.next = a
+            a.next = c
 
             prev = a
             cur = c
-            
+
         return newHead
 
-    def swap(self, a, b, c):
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        a, b, c = head, head.next, head.next.next
+
+        next = self.swapPairs(c)
+        
         b.next = a
-        a.next = c
-# 1 -> 2 -> 3 -> 4
-# 2 -> 1 -> 3 -> 4
-# 2 -> 1 -> 4 -> 3
+        a.next = next
         
-        
+        return b
