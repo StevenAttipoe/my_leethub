967516cb5f3406d64956136b221646efc4fe489a
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2024-09-06 01:07:08 +0000

    Time: 34 ms (80.09%), Space: 17.5 MB (8.14%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -8,10 +8,73 @@ class Node:
 """
 
 class Solution:
+    # O(N) time and O(1) space
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return head
-            
+        
+        # Sandwich original and copied nodes
+        cur = head
+        while cur:
+            copy = Node(cur.val)
+            copy.next = cur.next
+            cur.next = copy
+            cur = copy.next
+
+        # Set random points for copied nodes
+        cur = head
+        while cur:
+            copy = cur.next
+
+            if cur.random:
+                copy.random = cur.random.next
+            else:
+                copy.random = None
+
+            cur = copy.next
+
+        # Unweave original and copied nodes
+        copiedHead = head.next
+        originalHead = head
+        result = head.next
+        while originalHead:
+            originalHead.next = originalHead.next.next
+
+            if copiedHead.next:
+                copiedHead.next = copiedHead.next.next
+            else:
+                copiedHead.next = None
+
+            originalHead = originalHead.next
+            copiedHead = copiedHead.next
+
+        return result
+
+    # O(N) time and space. Cleaner approach
+    def copyRandomList2(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        hashmap = { None : None }
+
+        cur = head
+        while cur:
+            hashmap[cur] = Node(cur.val)
+            cur = cur.next
+
+        cur = head
+        while cur:
+            copiedNode = hashmap[cur]
+
+            copiedNode.next = hashmap[cur.next]
+            copiedNode.random = hashmap[cur.random]
+
+            cur = cur.next
+
+        return hashmap[head]
+
+    # O(N) time and space
+    def copyRandomList3(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return head
+
         copiedHead = Node(head.val)
         hashmap = {
             None : None,
@@ -29,10 +92,7 @@ class Solution:
 
             if head.random in hashmap:
                 cur.random = hashmap[head.random]
-                if hashmap[head.random]:
-                    print(cur.val, hashmap[head.random].val)
             else:
-                print(cur.val, "here")
                 copiedRandom = Node(head.random.val)
                 cur.random = copiedRandom
                 hashmap[head.random] = copiedRandom
