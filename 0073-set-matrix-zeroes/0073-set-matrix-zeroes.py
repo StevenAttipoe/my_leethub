62a9cccc6e9495df4d00c6536c3b97257beb7279
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2024-09-09 00:04:23 +0000

    Time: 105 ms (42.18%), Space: 17.4 MB (85.41%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,5 +1,56 @@
 class Solution:
+    # O(M * N) time and O(1) space
     def setZeroes(self, matrix: List[List[int]]) -> None:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        rowZero = False
+
+        # Determine which rows/cols to set to zero
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        rowZero = True
+
+        # Set rows/cols to zero
+        for r in range(1, ROWS):
+            for c in range(1, COLS):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+
+        # Set first col to zero if needed
+        if matrix[0][0] == 0:
+            for r in range(ROWS):
+                matrix[r][0] = 0
+        
+        # Set first row to zero if needed
+        if rowZero:
+            for c in range(COLS):
+                matrix[0][c] = 0
+
+    # O(M * N) time and O(M + N) space
+    def setZeroes2(self, matrix: List[List[int]]) -> None:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        rowSet, colSet = set(), set()
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if matrix[i][j] == 0:
+                    rowSet.add(i)
+                    colSet.add(j)
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if i in rowSet or j in colSet:
+                    matrix[i][j] = 0
+        
+
+
+    # O(M * N) time and O(M * N) space
+    def setZeroes3(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
