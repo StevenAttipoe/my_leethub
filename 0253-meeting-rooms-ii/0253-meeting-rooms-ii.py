0454d44ef4b3cbdf76ffae80f8fa88572949ed77
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2024-09-15 09:35:22 +0000

    Time: 82 ms (15.98%), Space: 19.7 MB (99.85%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -3,12 +3,12 @@ class Solution:
         intervals.sort(key = lambda x: x[0])
 
         rooms = []
-        heapq.heappush(rooms, (intervals[0][1], intervals[0]))
+        heapq.heappush(rooms, intervals[0][1])
 
         for i in intervals[1:]:
-            if rooms[0][1][1] <= i[0]:
+            if rooms[0] <= i[0]:
                 heapq.heappop(rooms)
 
-            heapq.heappush(rooms, (i[1], i))
+            heapq.heappush(rooms, i[1])
 
         return len(rooms)
