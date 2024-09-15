035765be39b09c04cf48be7752f1899a143ed6d4
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2024-09-15 11:20:26 +0000

    Time: 59 ms (50.01%), Space: 16.6 MB (71.3%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,5 +1,27 @@
 class Solution:
+    # O(n + klogn) time and O(n) space
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        wordFreq = {}
+
+        for w in words:
+            wordFreq[w] = wordFreq.get(w, 0) + 1
+
+        heap = [(-f, w) for w,f in wordFreq.items()]
+        heapq.heapify(heap)
+
+        return [heapq.heappop(heap)[1] for _ in range(k)]
+
+    # O(nlogn) time and O(n) space
+    def topKFrequent2(self, words: List[str], k: int) -> List[str]:
+        wordFreq = {}
+
+        for w in words:
+            wordFreq[w] = wordFreq.get(w, 0) + 1
+
+        result = sorted(wordFreq.keys(), key = lambda w: (-wordFreq[w], w))
+        return result[:k]
+
+    def topKFrequent2(self, words: List[str], k: int) -> List[str]:
         words.sort()
         wordFreq = {}
 
