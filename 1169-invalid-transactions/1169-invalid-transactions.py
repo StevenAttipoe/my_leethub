e3f3564cbd6c334f20a6570b3579e0942d8e2316
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2024-09-22 02:40:16 +0000

    Time: 63 ms (69.35%), Space: 17.6 MB (5.75%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -1,38 +1,38 @@
 class Solution:
-    def __init__(self):
-        self.records = {}
-        
-    def populateRecords(self, transactions: List[str]) -> None:
+    def invalidTransactions(self, transactions: List[str]) -> List[str]:
+        history = defaultdict(dict)
+        result = []
+
         for t in transactions:
             name, time, amount, city = t.split(",")
-            time = int(time)
-            if time in self.records:
-                if name in self.records[time]:
-                    self.records[time][name].append(city)
-                else:
-                    self.records[time][name] = [city]
-            else:
-                self.records[time] = {name:[city]}
-            
-    def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        self.populateRecords(transactions)
-        res = []
-        for trans in transactions:
-            name, time, amount, city = trans.split(",")
-            time = int(time)
-            if int(amount) > 1000:
-                res.append(trans)
-                continue
+            time, amount = int(time), int(amount)
 
-            for time in range(time - 60, time + 61):
-                if time not in self.records:
+            if name not in history[time]:
+                history[time][name] = {city}
+            else:
+                history[time][name].add(city)
+
+        for t in transactions:
+            name, time, amount, city = t.split(",")
+            time, amount = int(time), int(amount)
+            
+            if amount > 1000:
+                result.append(t)
+                continue
+            
+            for invalidTime in range(time - 60, time + 61):
+                if invalidTime not in history:
                     continue
-                if name not in self.records[time]:
+                if name not in history[invalidTime]:
                     continue
-                if len(self.records[time][name]) > 1 or self.records[time][name][0] != city:
-                    res.append(trans)
+
+                citySet = history[invalidTime][name]
+                
+                if city not in citySet or len(citySet) > 1:
+                    result.append(t)
                     break
 
-        return res
+        return result
 
-            
+
+        
