b6fdd2ead09992b9ecc6aa98d97df0a389b7fd3e
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2024-09-22 04:11:20 +0000

    Time: 123 ms (59.45%), Space: 20.5 MB (81.14%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,21 +4,17 @@ class Solution:
             return intervals
 
         intervals.sort(key = lambda x: x[0])
-        merged = [intervals[0]]
+        merged = []
 
-        for i in range(1, len(intervals)):
-            if self.isMergable(merged[-1], intervals[i]):
-                startInterval = merged.pop()
-                merged.append(self.mergeIntervals(startInterval, intervals[i]))
+        for i in intervals:
+            if not merged or not self.isMergable(merged[-1], i):
+                merged.append(i)
             else:
-                merged.append(intervals[i])
-
+                merged[-1][1] = max(merged[-1][1], i[1])
+                
         return merged
     
     def isMergable(self, intervals1, intervals2):
         return intervals1[1] >= intervals2[0]
-
-    def mergeIntervals(self, intervals1, intervals2):
-        return [intervals1[0], max(intervals1[1], intervals2[1])]
 
         
