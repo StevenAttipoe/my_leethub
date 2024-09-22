9b80bb61486ae84d7eb1d6eb8803163dbb34e109
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2024-09-22 03:41:08 +0000

    Time: 46 ms (34.48%), Space: 16.6 MB (34.4%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,28 +1,31 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        pivot = len(nums) - 2
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 2
+        while i >= 0 and nums[i + 1] <= nums[i]:
+            i -= 1
+
+        if i >= 0:
+            j = len(nums) - 1
+            while nums[i] >= nums[j]:
+                j -= 1
+            self.swap(i, j, nums)
         
-        #Find pivot, index of element that is non increasing
-        while pivot >= 0 and nums[pivot] >= nums[pivot + 1]:
-            pivot -= 1
+        self.reverse(i + 1, nums)
+    
+    def swap(self, i, j, nums):
+        nums[i], nums[j] = nums[j], nums[i]
+    
+    def reverse(self, s, nums):
+        e = len(nums) - 1
 
-        #Find next num greater than pivot num
-        if pivot >= 0:
-            nxt = len(nums) - 1
-            while nxt > 0 and nums[pivot] >= nums[nxt]:
-                nxt -= 1
-            self.swap(nums, pivot, nxt)
+        while s < e:
+            self.swap(s, e, nums)
+            s += 1
+            e -= 1
 
-        #Reverse subarray after pivot
-        l, r = pivot + 1, len(nums) - 1
-        while l < r:
-            self.swap(nums, l, r)
-            l += 1
-            r -= 1
+            
 
-    def swap(self, nums, l, r):
-        nums[l], nums[r] = nums[r], nums[l]
-
-
-        
         
