9860c50957c27866785d285ae811b0884a916a63
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2024-09-28 08:55:20 +0000

    Time: 99 ms (86.01%), Space: 17.8 MB (45.51%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,42 +1,13 @@
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        l = 0
-        r = len(numbers) - 1
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
 
-        while l < r:
-            if numbers[l] + numbers[r] == target:
-                return [l+1, r+1]
-            elif numbers[l] + numbers[r] < target:
-                l += 1
-            else:
-                r -= 1
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            if complement in seen:
+                return [seen[complement], i + 1]
+            
+            seen[nums[i]] = i + 1
 
         return [-1, -1]
-
-    def twoSum2(self, numbers: List[int], target: int) -> List[int]:
-        pairs = []
-
-        for a in range(len(numbers)):
-            b = self.search(a, target, numbers)
-            if b != None and a !=b:
-                return [a + 1, b + 1]
-
-    def search(self, a, target, nums):
-        l = a
-        r = len(nums) - 1
-
-        while l <= r:
-            mid = (l + r) // 2
-            if a == mid:
-                mid += 1
-            if nums[a] + nums[mid] < target:
-                l = mid + 1
-            elif nums[a] + nums[mid] > target:
-                r = mid - 1
-            else:
-                return mid
-
-        return None
-
-        
-        
