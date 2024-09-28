16225816f23ffd2b5133641b540f4e849a7ed88a
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2024-09-28 08:08:22 +0000

    Time: 788 ms (32.71%), Space: 20.9 MB (19.52%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,22 +1,45 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
         nums.sort()
-        distinctTriplets = set()        
+        result = []
+
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+                
+            j, k = i + 1, n - 1  
+            while j < k:
+                a, b, c = nums[i], nums[j], nums[k]
+                totalSum = a + b + c
+
+                if totalSum == 0:
+                    result.append([a,b,c])
+                    j += 1
+                    k -= 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+
+                elif totalSum > 0:
+                    k -= 1
+                else:
+                    j += 1
         
-        for i in range(len(nums) - 2):
-            l, r  = i + 1, len(nums) - 1
+        return result
+
+    # O(N^3) time
+    def threeSum2(self, nums: List[int]) -> List[List[int]]:
+        result = set()
+        nums.sort()
+        n = len(nums)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                for k in range(j + 1, n):
+                    if i != j or j != k or i != k:
+                        if nums[i] + nums[j] + nums[k] == 0:
+                            result.add((nums[i], nums[j], nums[k]))
+
+        return result
         
-            while l < r:
-                _sum = nums[i] + nums[l] + nums[r]
-                if _sum == 0:
-                    distinctTriplets.add((nums[i], nums[l], nums[r]))
-                    r -= 1
-                    l += 1
-                    
-                if _sum > 0:
-                    r -= 1
-                    
-                if _sum < 0:
-                    l += 1
-        return list(distinctTriplets)
-         
+        
