3cf8dfe9aec4da86e5809d01a4da838ff55a76fa
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2024-11-13 03:49:31 +0000

    Time: 18 ms (44.88%), Space: 18.2 MB (94.09%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,22 +1,41 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        l , r = 0 , len(height) - 1
-        maxLeft, maxRight = height[l], height[r]
-        res = 0
-        
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = -1, -1
+        totalAmount = 0
+
         while l < r:
-            if maxLeft < maxRight:
+            leftMax = max(leftMax, height[l])
+            rightMax = max(rightMax, height[r])
+
+            if leftMax < rightMax:
+                totalAmount += leftMax - height[l]
                 l += 1
-                amount = maxLeft - height[l]
-                if amount > 0:
-                    res += amount
-                maxLeft = max(maxLeft, height[l])
-                
             else:
+                totalAmount += rightMax - height[r]
                 r -= 1
-                amount = maxRight - height[r]
-                if amount > 0:
-                    res += amount
-                maxRight = max(maxRight, height[r])
         
-        return res
+        return totalAmount
+        
+    def trap2(self, height: List[int]) -> int:
+        n = len(height)
+        totalAmount = 0
+
+        leftMax = [0] * n
+        rightMax = [0] * n
+
+        leftMax[0] = height[0]
+        for i in range(1, n):
+            leftMax[i] = max(leftMax[i - 1], height[i])
+
+        rightMax[-1] = height[-1]
+        for i in range(n - 2, -1, -1):
+            rightMax[i] = max(rightMax[i + 1], height[i])
+
+        for i in range(n):
+            totalAmount += min(leftMax[i], rightMax[i]) - height[i]
+        
+        return totalAmount
+
+
+        
