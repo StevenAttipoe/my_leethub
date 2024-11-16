3c6925f49b377d92c51a36ff7dde10f053f3e442
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2024-11-16 16:59:31 +0000

    Time: 664 ms (68.42%), Space: 17 MB (58.41%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.py
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -1,24 +1,30 @@
 class Solution:
     def knightDialer(self, n: int) -> int:
-        DIALS = {0: [4, 6], 
-                1 : [6, 8], 
-                2 : [7, 9],
-                3 : [4, 8],
-                4 : [0, 3, 9],
-                5 : [],
-                6 : [0, 1, 7],
-                7 : [2, 6],
-                8 : [1, 3],
-                9 : [2, 4]}
+        if n == 1:
+            return 10
         
-        initialCount = [1] * 10
-        for i in range(n - 1):
-            curCount = [0] * 10
-            for src in range(10):
-                for des in DIALS[src]:
-                    curCount[des] += initialCount[src] 
-            initialCount = curCount
+        moves = [
+            [4, 6],
+            [6, 8],
+            [7, 9],
+            [4, 8],
+            [3, 9, 0],
+            [],
+            [1, 7, 0],
+            [2, 6],
+            [1, 3],
+            [2, 4]
+        ]
+
+        mod =  10 ** 9 + 7
+        dp = [1] * 10
+       
+        for _ in range(n - 1):
+            next_dp = [0] * 10
+            for i in range(10):
+                for m in moves[i]:
+                    next_dp[m] += dp[i]
+            dp = next_dp
         
-        return sum(initialCount) % (10 ** 9 + 7)
-            
+        return sum(dp) % mod
         
