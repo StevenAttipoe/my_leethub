af2f2028505ede3f5a826974c2e80a1d34058782
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2024-11-17 03:40:39 +0000

    Time: 9 ms (21.92%), Space: 16.6 MB (43.89%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,20 +1,24 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        SYMBOLS = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        val = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
 
-        num = i = 0
-        while i < len(s) - 1:
-            if SYMBOLS[s[i]] < SYMBOLS[s[i + 1]]:
-                num += SYMBOLS[s[i + 1]] - SYMBOLS[s[i]]
-                i += 2
+        digit = 0
+
+        for i, char in enumerate(s):
+            if i < len(s) - 1 and val[char] < val[s[i + 1]]:
+                digit -= val[char]
             else:
-                num += SYMBOLS[s[i]]
-                i += 1
+                digit += val[char]
 
-        if i < len(s):
-            num += SYMBOLS[s[i]]
-
-        return num
+        return digit
 
 
         
