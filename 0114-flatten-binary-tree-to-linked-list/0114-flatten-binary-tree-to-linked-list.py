51acfe0fa2d8a8ad0a0b2885aab6a1d8bd58e9f2
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2024-11-20 00:24:30 +0000

    Time: 0 ms (100%), Space: 16.8 MB (46.24%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -5,22 +5,48 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.prev = None
+
     def flatten(self, root: Optional[TreeNode]) -> None:
         if not root:
-            return root
+            return
 
         stack = [root]
+
         while stack:
             node = stack.pop()
-            
+
             if node.right:
                 stack.append(node.right)
-
+            
             if node.left:
                 stack.append(node.left)
                 node.right = node.left
                 node.left = None
+            
             else:
                 if stack:
                     node.right = stack[-1]
+
+    def flatten2(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        
+        def dfs(node):
+            if not node:
+                return node
+
+            dfs(node.right)
+            dfs(node.left)
+
+            node.right = self.prev
+            node.left = None
+            self.prev = node
+        
+        dfs(root)
+
+
+
         
