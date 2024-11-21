cd5812418433b3d2ff69b4af16e6590e181e250c
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2024-11-21 20:19:14 +0000

    Time: 0 ms (100%), Space: 16.8 MB (16.96%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,5 +1,21 @@
 class Solution:
+    # O(N * 2^N) time
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        powerSet = []
+
+        def backtrack(i, listSet):
+            powerSet.append(listSet[:])
+            
+            for i in range(i, len(nums)):
+                listSet.append(nums[i])
+                backtrack(i + 1, listSet)
+                listSet.pop()
+
+        backtrack(0, [])
+
+        return powerSet
+        
+    def subsets2(self, nums: List[int]) -> List[List[int]]:
         powerSet = []
 
         def backtrack(i, listSet):
