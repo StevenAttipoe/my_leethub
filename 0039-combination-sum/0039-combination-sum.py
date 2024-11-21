b60243bf8838691166a909ad635f61a28e9f69b3
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2024-11-21 23:48:34 +0000

    Time: 27 ms (12.55%), Space: 16.7 MB (41.2%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,35 +1,22 @@
 class Solution:
-    # Time Complexity: O(N^(T/M+1))
-    # Space Complexity: O(T/M)
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        results = []
+        result = []
 
-        def backtrack(remain, comb, start):
-            if remain == 0:
-                results.append(list(comb))
-                return
-            if remain < 0:
+        def backtrack(s, comb):
+            if sum(comb) == target:
+                result.append(comb[:])
                 return
 
-            for i in range(start, len(candidates)):
-                comb.append(candidates[i])
-                backtrack(remain - candidates[i], comb, i)
-                comb.pop()
+            if s >= len(candidates) or sum(comb) > target:
+                return
 
-        backtrack(target, [], 0)
-        return results
+            comb.append(candidates[s])
+            backtrack(s, comb)
+            comb.pop()
+            backtrack(s + 1, comb)
 
-    # Time Complexity: O(M*M*N), N = len(candidates), M = target
-    # Space Complexity: O(M*M)
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        dp = [[] for _ in range(target+1)]
-        for c in candidates: # O(N): for each candidate
-            for i in range(c, target+1): # O(M): for each possible value <= target
-                if i == c: 
-                    dp[i].append([c])
-                for comb in dp[i-c]: 
-                    dp[i].append(comb + [c]) # O(M) worst: for each combination
-        return dp[-1]
-
-
+        backtrack(0, [])
+        return result
+        
+        
         
