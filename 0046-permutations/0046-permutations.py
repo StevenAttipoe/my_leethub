1efa70c4c19da66143fbafa330be425aa78bcbaa
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2024-11-21 20:04:59 +0000

    Time: 0 ms (100%), Space: 16.8 MB (33.21%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,58 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        permutations = []
+        result = []
 
-        def helper(permutation):
-            if len(permutation) == len(nums):
-                permutations.append(permutation[:])
+        def backtrack(start):
+            if start == len(nums):
+                result.append(nums[:])
                 return
 
-            for num in nums:
-                if num not in permutation:
-                    permutation.append(num)
-                    helper(permutation)
-                    permutation.pop() #recursively pop (x2)
+            for i in range(start, len(nums)):
+                nums[i], nums[start] = nums[start], nums[i]
+                backtrack(start + 1)
+                nums[i], nums[start] = nums[start], nums[i]
 
-        helper([])
-        
-        return permutations
-
-
-    def permute2(self, nums: List[int]) -> List[List[int]]:
-        permutations = []
-
-        def helper(i):
-            if i == len(nums):
-                permutations.append(nums[:])
-                return
-
-            for j in range(i, len(nums)):
-                nums[i], nums[j] = nums[j], nums[i]
-                helper(i + 1)
-                nums[i], nums[j] = nums[j], nums[i]
-
-        helper(0)
-        
-        return permutations
-
-    def permute3(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        count = Counter(nums)
-
-        def helper(cur):
-            if len(cur) == len(nums):
-                res.append(cur)
-                return
-
-            for num in count.keys():
-                if count[num]:
-                    count[num] -= 1
-                    helper(cur + [num])
-                    count[num] += 1
-
-        helper([])
-        return res
-
-        
-
-        
+        backtrack(0)
+        return result    
