1c32835d2db261f044b3df794a1af92cf3022e51
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2024-12-30 01:32:38 +0000

    Time: 147 ms (56.01%), Space: 18.1 MB (41.08%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -10,11 +10,28 @@ class Solution:
             steps += 1
         return steps
 
+    def getPower(self, x, memo, steps):
+        if x == 1:
+            return steps
+
+        if x % 2 == 0:
+            x = x // 2
+        else:
+            x = 3 * x + 1
+
+        steps += 1
+        if x in memo:
+            return steps + memo[x]
+        
+        return self.getPower(x, memo, steps)
+
     def getKth(self, lo: int, hi: int, k: int) -> int:
         powerVals = {}
+        memo = {}
 
         for i in range(lo, hi + 1):
-            p = self.getPower(i)
+            p = self.getPower(i,memo, 0)
+            memo[i] = p
             powerVals[i] = p
         
         return sorted(powerVals.keys(), key = lambda x: powerVals[x])[k - 1]
