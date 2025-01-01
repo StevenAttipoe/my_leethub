de7ed84cd4c51b047cf58aab8e9e07cd94f1a21e
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2025-01-01 14:52:18 +0000

    Time: 7 ms (34.38%), Space: 19.2 MB (9.35%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,6 +1,28 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         numFreq = {}
+
+        for num in nums:
+            numFreq[num] = numFreq.get(num, 0) + 1
+
+            if len(numFreq) <= 2:
+                continue
+            
+            newNumFreq = {}
+            for n, f in numFreq.items():
+                if f > 1:
+                    newNumFreq[n] = f - 1
+            numFreq = newNumFreq
+        
+        result = []
+        for n in numFreq:
+            if nums.count(n) > len(nums) // 3:
+                result.append(n)
+
+        return result
+
+    def majorityElement2(self, nums: List[int]) -> List[int]:
+        numFreq = {}
         n = len(nums)
 
         for num in nums:
