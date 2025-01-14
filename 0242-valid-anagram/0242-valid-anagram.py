bbf422ddea86f993a983d7db66981d7cb3eda1b9
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2025-01-14 01:16:44 +0000

    Time: 7 ms (90.74%), Space: 18 MB (20.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        charFreq1 = [0] * 26
+        charFreq2 = [0] * 26
+
+        for char in s:
+            charFreq1[ord(char) - ord('a')] += 1
+        
+        for char in t:
+            charFreq2[ord(char) - ord('a')] += 1
+        
+        return charFreq1 == charFreq2
+        
