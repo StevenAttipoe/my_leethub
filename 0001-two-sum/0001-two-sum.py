c1a957ad7f3af625d09161f09c5cf0f1c12a65b3
Steven <60293933+StevenAttipoe@users.noreply.github.com>
2025-04-14 05:13:17 +0000

    Time: 0 ms (100%), Space: 18.9 MB (51.63%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,27 +1,23 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numsDict = {}            
+        N = len(nums)
+        for i in range(N):
+            for j in range(i + 1, N):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
         
-        for i in range(len(nums)):
-            dif = target - nums[i]
-            
-            if dif in numsDict:
-                return [i, numsDict[dif]]
-
-            numsDict[nums[i]] = i
-
-                
-    def twoSum2(self, nums: List[int], target: int) -> List[int]:
-        numsDict = {}
-        
-        for i in range(len(nums)):
-            numsDict[nums[i]] = i
-            
-        
-        for i in range(len(nums)):
-            dif = target - nums[i]
-            
-            if dif in numsDict and i != numsDict[dif]:
-                return [i, numsDict[dif]]
-                
         return [-1, -1]
+    
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numsMap = {}
+
+        for i, n in enumerate(nums):
+            numsMap[n] = i
+        
+        for i, n in enumerate(nums):
+            dif = target - n
+            if dif in numsMap and numsMap[dif] != i:
+                return [i, numsMap[dif]]
+        
+        return [-1, -1]
+        
